e997a1e711d3556b7165548643eed59b670e3cfc
Your Name <you@example.com>
2026-04-09 14:41:29 +0530

    Fix imports: add fast-alpr and fix unified_detection_module paths

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,14 +4,7 @@ Contains utility functions and helper classes.
 """
 
 from .color_detector import ColorDetector
-from .file_utils import FileUtils
-from .image_utils import ImageUtils
-from .logger import setup_logger, get_logger
 
 __all__ = [
-    'ColorDetector',
-    'FileUtils',
-    'ImageUtils',
-    'setup_logger',
-    'get_logger'
+    'ColorDetector'
 ]
